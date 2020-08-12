a6ff640ac03a6a01836b3cd8e3c97b861dc340ad
rafa <rcuestas@jaumebalmes.net>
2020-08-12 18:44:33 +0200

    Fin semana 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DestinioViajeComponent } from './destinio-viaje/destinio-viaje.component';
+import { ListaDestinoComponent } from './lista-destino/lista-destino.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DestinioViajeComponent,
+    ListaDestinoComponent
   ],
   imports: [
     BrowserModule,
